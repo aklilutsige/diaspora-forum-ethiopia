c875d711745e8e43238d6587088293fed2717d56
Aklilu <aklilutsige@gmail.com>
2021-05-22 20:57:06 +0200

    DAO implementation using Spring DATA JAP

--- src/main/java/et/ethiopia_info/administrative_divisions/repositories/IRegionRepository.java
+++ src/main/java/et/ethiopia_info/administrative_divisions/repositories/IRegionRepository.java
@@ -2,5 +2,7 @@ package et.ethiopia_info.administrative_divisions.repositories;
 
 import et.ethiopia_info.administrative_divisions.models.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface IRegionRepository extends JpaRepository<Region, Integer> { }
